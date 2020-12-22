7742ccaea7043a071e9597ea973679c1b03b8e58
Lalatendu Guru <lalatenduguru@Lalatendus-MacBook-Air.local>
2020-12-22 20:22:07 +0530

    Add service

--- src/main/java/com/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/JenkinsApplication.java
@@ -2,12 +2,19 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
-
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Test application successful!!";
+	}
 }
